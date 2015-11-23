5d9c6da95a3b49b44df1f6f45d4b103ba78b0946
Alex Pennells <alex.pennells@vidyard.com>
2015-11-22 22:29:32 -0500

    added drawing constants to stitchin config

--- Assets/Scripts/stitchinConfig/Stitch.cs
+++ Assets/Scripts/stitchinConfig/Stitch.cs
@@ -2,6 +2,21 @@ using UnityEngine;
 
 public class Stitch : MonoBehaviour
 {
+  /***********************************
+   * DRAWING CONSTANTS
+   **********************************/
+
+  public static bool SHOW_GAME_GRID = false;
+  public static bool SHOW_OBJ_ORIGIN = true;
+  public static bool SHOW_MASK_BOUNDS = false;
+  public static bool SHOW_TERRAIN_BOUNDS = true;
+  public static bool SHOW_RAIL_PATHS = true;
+  public static bool SHOW_CAMERA_BOUNDS = true;
+  public static bool SHOW_EXIT_BOUNDS = true;
+
+  /***********************************
+   * GAME CONSTANTS
+   **********************************/
 
   void Awake() {
     instance = this;
