de57fd1c124417cf84fd1f627251627f811a6b3b
Alex Pennells <alex.pennells@vidyard.com>
2015-12-24 20:06:32 -0500

    Kat now bounces on ground pound miss

--- Assets/Scripts/Kat/Kat_SolidCollider.cs
+++ Assets/Scripts/Kat/Kat_SolidCollider.cs
@@ -1,16 +1,44 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class Kat_SolidCollider : SolidColliderObj {
+  private bool bounced = false;
+
+  public override void HandleCollisions (List<SolidObj> others) {
+    base.HandleCollisions(others);
+    if (bounced)
+      ClearFooting();
+  }
+
   protected override void FootingCollision(SolidObj footing) {
     if (Base.Physics.vspeed != 0)
       Base.Sound.Play("Land");
 
     base.FootingCollision(footing);
 
-    (Base as Kat_Base).hasAirAttack = true;
-    (Base as Kat_Base).hasUppercut = true;
+    bounced = false;
+    if (Base.Sprite.IsPlaying("kat_pound"))
+      BounceOffGround();
+
+    Stitch.Kat.hasAirAttack = true;
+    Stitch.Kat.hasUppercut = true;
 
     if (Base.Sprite.IsPlaying("kat_uppercut"))
       Base.Sprite.Play("Idle");
+  }
+
+  private void BounceOffGround () {
+    Stitch.Kat.stopPhysics = false;
+    Stitch.Kat.AirKickTimer.Enabled = false;
+    Stitch.Kat.GroundKickTimer.Enabled = false;
+    Stitch.Kat.hasAirAttack = true;
+    Stitch.Kat.hasUppercut = true;
+
+    Base.Physics.hspeed = 0;
+    Base.Physics.vspeed = 7;
+
+    Base.Sprite.Play("Spin");
+    bounced = true;
   }
 }
